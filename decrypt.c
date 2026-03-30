0c9205b0f8620e5346e326f57a9f545aeed1157a
michiaguilarr <michiaguilarr@gmail.com>
2026-03-30 20:37:22 -0300

    funciona

--- decrypt.c
+++ decrypt.c
@@ -10,21 +10,16 @@ int main(int argc, char *argv[]) {
   ssize_t n = read(0, buf, sizeof(buf));
   buf[n] = '\0'; 
   char *linea = buf;
+  size_t largo = strlen(linea);
 
  
-  size_t largo = strlen(linea);
-  int j = 0;
-  size_t largoResultado = (largo / 8) + 1;
 
-  char *result = malloc(largoResultado * sizeof(char));
 
   printf("recibido: %s\n", linea);
-  for (size_t i = inicio; i < largo; i += 7) {
+  for (size_t i = inicio; i < largo; i += 8) {
     write(1, &linea[i],1);
-    j++;
+
   }
-  result[j] = '\0';    
-   write(1, result,j);
-   write(1, "\n", 1);
+
   exit(EXIT_SUCCESS);
 }
